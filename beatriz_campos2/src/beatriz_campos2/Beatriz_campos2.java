8c679e2849fe59139aa7d2706a9e7c3a11d3e436
BeatrizPCamposs <101807045+BeatrizPCamposs@users.noreply.github.com>
2022-03-17 09:29:59 -0300

    Exercício 1 - Java
    Resolução - Exercício 1

---
+++ beatriz_campos2/src/beatriz_campos2/Beatriz_campos2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beatriz_campos2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 2etimds
+ */
+public class Beatriz_campos2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     
+        Scanner entrada = new Scanner(System.in);
+        
+    }
+    
+}
